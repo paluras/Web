0048a178539b1eb851319a4384ff3e102601da1e
paluras <paluraandrei1@gmail.com>
2022-11-02 15:05:32 +0200

    Battlle and animations

--- classes.js
+++ classes.js
@@ -4,7 +4,8 @@ class Sprite{
         velocity,
         image,
         frames = {max: 1},
-        sprites
+        sprites,
+        isEnemy = false
     }) {
         this.position = position;
         this.image = image
@@ -16,9 +17,14 @@ class Sprite{
         } 
         this.moving =  false
         this.sprites = sprites
+        this.opacity = 1
+        this.health = 100
+        this.isEnemy =  isEnemy
     }
 
     draw(){
+        c.save()
+        c.globalAlpha = this.opacity
         c.drawImage(
             this.image,
             //71 pixel to move the sprite sheet, its the widthof one
@@ -31,6 +37,7 @@ class Sprite{
             this.image.width / this.frames.max ,
             this.image.height 
         )  
+        c.restore()
             if(!this.moving)  
              return this.frames.val = 0
             if(this.frames.max > 1){
@@ -41,6 +48,59 @@ class Sprite{
             else this.frames.val = 0
             }
     }   
+
+   attack ({ attack, recipient}){
+
+    switch(attack.name) {
+        case'Water':
+
+        break;
+
+        
+        case 'Flap' :
+            const tl = gsap.timeline()
+
+            this.health -= attack.damage
+        
+            let movementDistance = 20
+            if(this.isEnemy) movementDistance = -20
+        
+            let healthbar = '#enemyHealth'
+            if(this.isEnemy) healthbar = '#ownHealth'
+            
+            tl.to(this.position, {
+                x:this.position.x -  movementDistance
+            }).to(this.position, {
+                x:this.position.x + movementDistance * 2,
+                duration: 0.1,
+                onComplete: () => {
+        
+                    gsap.to(healthbar, {
+                        width: this.health  - attack.damage + "%"
+                    })
+                    gsap.to(recipient.position, {
+                        x: recipient.position.x + 10,
+                        yoyo: true,
+                        repeat: 5,
+                        duration: 0.08,
+                    })
+        
+                    gsap.to(recipient ,{
+                        opacity: 0.5,
+                        repeat: 5,
+                        yoyo: true,
+                        duration: 0.08
+                    })
+                }
+            }).to(this.position, {
+                x:this.position.x 
+            })
+        
+
+        break;
+    }
+    
+   }
 }
 
 
